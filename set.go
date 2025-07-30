1e6a4cbd73cb8411aac2c24af9a08adf48e0e2d4
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2025-07-30 10:54:24 +0200

    feat: finalize Set interface

--- set.go
+++ set.go
@@ -1,16 +1,21 @@
 // Package threadsafe implements thread-safe operations.
 package threadsafe
 
-type Set[T any] interface {
-    // Add stores an item in the set.
-    Add(item T)
-    // Remove deletes an item from the set.
-    Remove(item T)
-    // Has returns true if the item is in the set, otherwise false.
-    Has(item T) bool
-    // Len returns the number of items in the set.
-    Len() int
-    // Clear removes all items from the set.
-    Clear()
+// Set is a generic interface for a set store of any type T.
+type Set[T comparable] interface {
+	// Add stores an item in the set.
+	Add(item T)
+	// Remove deletes an item from the set.
+	Remove(item T)
+	// Has returns true if the item is in the set, otherwise false.
+	Has(item T) bool
+	// Len returns the number of items in the set.
+	Len() int
+	// Clear removes all items from the set.
+	Clear()
+	// Slice returns a copy of the set as a slice.
+	Slice() []T
+	// Range calls f sequentially for each item present in the set.
+	// If f returns false, range stops the iteration.
+	Range(f func(item T) bool)
 }
-
